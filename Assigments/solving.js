247e6bbd5ffdbb78700bcc5e18fe2cef24ab1099
anjali-tripathi1 <anjalitripathi.kit@gmail.com>
2025-09-22 15:31:19 +0530

    upload

--- Assigments/solving.js
+++ Assigments/solving.js
@@ -192,7 +192,7 @@ console.log(calcTip(100));
 
 const bills = [125, 555, 44];
 
-const tips =  [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+const tips =  [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 
 const totals = bills.map((bill, i) => bill + tips[i]);
 

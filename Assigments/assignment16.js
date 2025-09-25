a81bafde25eda45917b530a3ed9e20b7bbe0f6cd
anjali-tripathi1 <anjalitripathi.kit@gmail.com>
2025-09-25 19:52:03 +0530

    upload

---
+++ Assigments/assignment16.js
@@ -0,0 +1,12 @@
+// Assignment16:Create an object called myCountry for a country of your choice, containing properties country, capital, language, population and neighbours (an array like we used in previous assignments).
+
+
+let myCountry = {
+     country : 'India',
+     capital : 'Delhi',
+     language : 'Hindi',
+     population : 1408.0,   // million
+     neighbours : ['Bangladesh', 'Nepal', 'China']
+}
+
+console.log(myCountry);
---
+++ Assigments/assignment17.js
@@ -0,0 +1,19 @@
+// Assignment17:1)Using the object from the previous assignment, log a string like this to the console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries and a capital called Helsinki'.
+
+// 2)Increase the country's population by two million using dot notation, and then decrease it by two million using bracket notation.
+
+
+let myCountry = {
+     country : 'India',
+     capital : 'Delhi',
+     language : 'Hindi',
+     population : 1408.0,   // million
+     neighbours : ['Bangladesh', 'Nepal', 'China']
+}
+
+console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries are [${myCountry.neighbours}] and a capital called ${myCountry.capital}.`);
+
+
+myCountry.population += 2  // million
+console.log(myCountry.population);
+ 
